64b4c70108cb862d225f4aa6eff85eb460b7783b
Kristaps Grinbergs <kristaps@qminderapp.com>
2017-02-07 13:13:02 +0200

    Fixes #51
    Websocket tests

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Run Node.JS Websockets test server
 ./run-node-test-app.sh
 
 set -o pipefail && xcodebuild -workspace Example/QminderTVApp.xcworkspace -scheme QminderTVApp -destination 'platform=tvOS Simulator,name=Apple TV 1080p' CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO clean test build | xcpretty
