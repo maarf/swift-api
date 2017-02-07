ce43d0cbcfdc05a77ded80f87f4c32090ea118f2
Kristaps Grinbergs <kristaps@qminderapp.com>
2017-02-07 14:13:23 +0200

    #51

--- Example/Pods/Target Support Files/Pods-QminderTVApp-QminderApiTests/Pods-QminderTVApp-QminderApiTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_QminderTVApp_QminderApiTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_QminderTVApp_QminderApiTestsVersionString[];
-
