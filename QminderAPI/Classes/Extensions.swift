c08e110bbedb7c92dfe4d52824d4e5e778b32575
Kristaps Grinbergs <kristaps@qminderapp.com>
2017-10-09 16:48:15 +0300

    Allow warnings

--- QminderAPI/Classes/Extensions.swift
+++ QminderAPI/Classes/Extensions.swift
@@ -57,7 +57,7 @@ extension String {
    */
   static func random(length: Int) -> String {
     let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    let randomLength = UInt32(letters.characters.count)
+    let randomLength = UInt32(letters.count)
     
     let randomString: String = (0 ..< length).reduce(String()) { accum, _ in
       let randomOffset = arc4random_uniform(randomLength)
