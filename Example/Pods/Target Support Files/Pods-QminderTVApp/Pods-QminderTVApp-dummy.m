85b71c4d53aee9967aff7309cb3cf4191d64632d
Kristaps Grinbergs <kristaps@qminderapp.com>
2017-12-22 10:34:41 +0200

    #112
    * remove pods
    * install pods from Circle

--- Example/Pods/Target Support Files/Pods-QminderTVApp/Pods-QminderTVApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QminderTVApp : NSObject
-@end
-@implementation PodsDummy_Pods_QminderTVApp
-@end
--- Example/Pods/Target Support Files/QminderAPI/QminderAPI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_QminderAPI : NSObject
-@end
-@implementation PodsDummy_QminderAPI
-@end
