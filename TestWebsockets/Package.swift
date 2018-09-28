f00323dc13472f8ab65609ee78f56c5c340a4887
Kristaps Grinbergs <fassko@gmail.com>
2018-09-28 15:21:16 +0300

    Fix unit tests

--- TestWebsockets/Package.swift
+++ TestWebsockets/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "TestWebsockets",
     dependencies: [
-        .package(url: "https://github.com/vapor/websocket.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/http.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/websocket.git", from: "1.0.2"),
+        .package(url: "https://github.com/vapor/http.git", from: "3.1.4"),
     ],
     targets: [
         .target(
